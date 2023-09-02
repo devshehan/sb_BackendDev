ec59dc027eb360b2d1ece52aaeaa8e10e38c91cc
devshehan <shehaan.avishka00@gmail.com>
2023-09-02 21:23:39 +0530

    StudentConfig file create to add data into the database

---
+++ src/main/java/com/devshehan/sbbackendDev/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.devshehan.sbbackendDev.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository){
+        return  args -> {
+            Student x  = new Student(
+                    "Jack",
+                    "jack@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 24),
+                    23
+            );
+
+            Student y  = new Student(
+                    "Rose",
+                    "Rose@gmail.com",
+                    LocalDate.of(2002, Month.FEBRUARY, 7),
+                    23
+            );
+            repository.saveAll(
+                    List.of(x,y)
+            );
+        };
+    }
+}

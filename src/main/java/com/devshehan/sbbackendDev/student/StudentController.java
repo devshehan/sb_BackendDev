26b9995c8e88fced6408475c45b4fec2c9ce9d53
devshehan <shehaan.avishka00@gmail.com>
2023-09-02 17:42:29 +0530

    student controller

--- src/main/java/com/devshehan/sbbackendDev/student/StudentController.java
+++ src/main/java/com/devshehan/sbbackendDev/student/StudentController.java
@@ -3,12 +3,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/student")
+@RequestMapping(path = "api/student")
 public class StudentController {
 
     private final StudentService studentService;

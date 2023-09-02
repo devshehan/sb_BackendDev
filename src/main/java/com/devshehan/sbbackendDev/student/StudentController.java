523c79ba9f970a519a99b41378b9082a87a0d2df
devshehan <shehaan.avishka00@gmail.com>
2023-09-02 17:38:16 +0530

    studentController connect to the studentService

--- src/main/java/com/devshehan/sbbackendDev/student/StudentController.java
+++ src/main/java/com/devshehan/sbbackendDev/student/StudentController.java
@@ -13,8 +13,8 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    public StudentController(StudentService studentService){
-        this.studentService = studentService;
+    public StudentController(){
+        this.studentService = new StudentService();
     }
 
     @GetMapping("/")

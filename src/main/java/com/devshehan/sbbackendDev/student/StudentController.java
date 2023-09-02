43309b53c9d008c62903bb2c22c260184e0d558f
devshehan <shehaan.avishka00@gmail.com>
2023-09-02 17:59:22 +0530

    autowired controller and service

--- src/main/java/com/devshehan/sbbackendDev/student/StudentController.java
+++ src/main/java/com/devshehan/sbbackendDev/student/StudentController.java
@@ -1,4 +1,5 @@
 package com.devshehan.sbbackendDev.student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -6,13 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/student")
+@RequestMapping(path =  "api/student")
 public class StudentController {
-
     private final StudentService studentService;
 
-    public StudentController(){
-        this.studentService = new StudentService();
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
     }
 
     @GetMapping("/")
--- src/main/java/com/devshehan/sbbackendDev/student/StudentService.java
+++ src/main/java/com/devshehan/sbbackendDev/student/StudentService.java
@@ -1,16 +1,19 @@
 package com.devshehan.sbbackendDev.student;
 
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
 
     public List<Student> getStudents(){
         return List.of(
                 new Student(
                         1L,
-                        "Shehaan",
+                        "Shehan",
                         "devshehan@gmail.com",
                         LocalDate.of(2000, Month.JANUARY,24),
                         24
